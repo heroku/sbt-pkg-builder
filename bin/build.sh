e3fabd97ec416b5a216c326669685836a3d1f8bb
Joe Kutner <jpkutner@gmail.com>
2014-09-08 18:45:14 -0500

    some changes to the build script

--- bin/build.sh
+++ bin/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-DRY_RUN=$4
-INCLUDES=$3
-EXCLUDES=$2
-ARCHIVE_NAME=$1
+DRY_RUN=$3
+ARCHIVE_NAME=$2
+PROJECTS_DIR=$1
 
 if [ -z "$ARCHIVE_NAME" ]; then
   echo "Archive name must be provided as first arg!"
@@ -12,10 +11,10 @@ if [ -z "$ARCHIVE_NAME" ]; then
 fi
 
 if [ -d target ] ; then
-  rm -rf target 
+  rm -rf target
 fi
 
-HOME_DIR=$(cd $(dirname $0)/..; pwd) 
+HOME_DIR=$(cd $(dirname $0)/..; pwd)
 BUILD_DIR=$HOME_DIR/target
 
 mkdir $BUILD_DIR
@@ -25,32 +24,28 @@ mkdir -p .ivy2/cache .sbt
 
 SBT_JAR="sbt-launch.jar"
 SBT_URL="http://typesafe.artifactoryonline.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.5/$SBT_JAR"
-curl -O --location $SBT_URL 
+curl -O --location $SBT_URL
 
 SBT_OPTS="-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled"
 SBT_OPTS="$SBT_OPTS -XX:MaxPermSize=256M -Dfile.encoding=UTF8"
 SBT_OPTS="$SBT_OPTS -Duser.home=$BUILD_DIR -Dsbt.global.base=$BUILD_DIR/.sbt"
 SBT_OPTS="$SBT_OPTS -Dsbt.ivy.home=$BUILD_DIR/.ivy2 -Divy.default.ivy.user.dir=$BUILD_DIR/.ivy2"
 
-PROJECT_DIRS=`ls $HOME_DIR/projects`
+PROJECT_DIRS=`ls $PROJECTS_DIR`
 for DIR in $PROJECT_DIRS; do
-  if [[ $DIR =~ $EXCLUDES ]]; then
-    echo "Skipping $DIR"
-  elif [[ $DIR =~ $INCLUDES ]]; then
-    echo "Resolving $DIR"
-    cp -r $HOME_DIR/projects/$DIR $DIR
-    cd $DIR
-    if [ -z "$DRY_RUN" ]; then
-      java $SBT_OPTS -jar $BUILD_DIR/$SBT_JAR update
-    fi
-    cd - >/dev/null 2>&1
-    rm -rf $DIR
-  else
-    echo "Skipping $DIR"
+  echo "Resolving $DIR"
+  cp -r $PROJECTS_DIR/$DIR $DIR
+  cd $DIR
+  if [ -z "$DRY_RUN" ]; then
+    java $SBT_OPTS -jar $BUILD_DIR/$SBT_JAR update
   fi
+  cd - >/dev/null 2>&1
+  rm -rf $DIR
 done
 
-tar cfz $ARCHIVE_NAME.tar.gz .ivy2 .sbt 
+rm -rf $PROJECTS_DIR/*
+
+tar cfz $ARCHIVE_NAME.tar.gz .ivy2 .sbt
 mv $ARCHIVE_NAME.tar.gz $HOME_DIR
 
 echo "done"

e12bd776b2a4d646c53dd1577e201d03f9ae4596
Joe Kutner <jpkutner@gmail.com>
2014-09-09 15:31:12 -0500

    switching to built-in travis s3 support

--- travis-build.sh
+++ travis-build.sh
@@ -3,19 +3,6 @@
 # Travis only needs to build the latest
 ./bin/build-play23.sh
 
-SCALA_VERSIONS="2.9 2.10 2.11"
-for SCALA_VERSION in $SCALA_VERSIONS; do
-  PLAY_VERSIONS="2.0 2.1 2.2 2.3"
-  for PLAY_VERSION in $PLAY_VERSIONS; do
-    CACHE_FILE="sbt-cache-play-${PLAY_VERSION}_${SCALA_VERSION}.tar.gz"
-    if [ -f $CACHE_FILE ]; then
-      echo "Uploading $CACHE_FILE..."
-      s3cmd \
-        --access_key="$AWS_ACCESS_KEY" \
-        --secret_key="$AWS_SECRET_KEY" \
-        put --acl-public \
-        $CACHE_FILE \
-        s3://lang-jvm.s3.amazonaws.com/sbt/stage/
-    fi
-  done
-done
+mkdir -p builds
+
+mv *.tar.gz builds

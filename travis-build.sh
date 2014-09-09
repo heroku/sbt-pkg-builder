dd968db8753bcab659464aa6451faf8b4f7fe145
Joe Kutner <jpkutner@gmail.com>
2014-09-08 21:00:43 -0500

    tweaked s3cmd options

--- travis-build.sh
+++ travis-build.sh
@@ -9,9 +9,10 @@ for SCALA_VERSION in $SCALA_VERSIONS; do
     CACHE_FILE="sbt-cache-play_${PLAY_VERSION}_${SCALA_VERSION}.tar.gz"
     if [ -f $CACHE_FILE ]; then
       echo "Uploading $CACHE_FILE..."
-      s3cmd put --acl-public \
+      s3cmd \
         --access_key="$AWS_ACCESS_KEY" \
         --secret_key="$AWS_SECRET_KEY" \
+        put --acl-public \
         $CACHE_FILE \
         s3://lang-jvm.s3.amazonaws.com/sbt/stage/
     fi

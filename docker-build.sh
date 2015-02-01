30950b8e0df358d82b2a9d6c21ee8f0a58391a7b
Joe Kutner <jpkutner@gmail.com>
2015-02-01 12:39:19 -0600

    added play 2.4 to docker build

--- docker-build.sh
+++ docker-build.sh
@@ -19,7 +19,7 @@ docker cp $CONTAINER_ID:/app/sbt-cache-base.tar.gz .
 
 SCALA_VERSIONS="2.9 2.10 2.11"
 for SCALA_VERSION in $SCALA_VERSIONS; do
-  PLAY_VERSIONS="2.0 2.1 2.2 2.3"
+  PLAY_VERSIONS="2.0 2.1 2.2 2.3 2.4"
   for PLAY_VERSION in $PLAY_VERSIONS; do
     echo "Copying sbt-cache-play-${PLAY_VERSION}_${SCALA_VERSION}.tar.gz..."
     docker cp $CONTAINER_ID:/app/sbt-cache-play_${PLAY_VERSION}_${SCALA_VERSION}.tar.gz .
